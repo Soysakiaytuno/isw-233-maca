582447e47f4bb9fae996d620db4bacfe0d4f1bd4
Sakia <leonardhopst@gmail.com>
2026-02-15 14:06:54 -0400

    agregando funcionalidad al header

--- Portafolio/js/main.js
+++ Portafolio/js/main.js
@@ -1,12 +1,32 @@
 const introduccion = document.querySelector('.textos-principales');
 const galeryproject = document.querySelector('.proyectos');
+const btnInicio = document.querySelector('#btn-inicio');
+const btnGaleria = document.querySelector('#btn-galeria');
+
+function mostrarProyectos() {
+    introduccion.classList.add('rotar-salida');
+    galeryproject.classList.add('rotar-entrada');
+}
+
+function mostrarInicio() {
+    introduccion.classList.remove('rotar-salida');
+    galeryproject.classList.remove('rotar-entrada');
+}
+
 window.addEventListener('wheel', (event) => {
     if (event.deltaY > 0) {
-        introduccion.classList.add('rotar-salida');
-        galeryproject.classList.add('rotar-entrada');
+        mostrarProyectos();
+    } else if (event.deltaY < 0) {
+        mostrarInicio();
     }
-    else if (event.deltaY < 0) {
-        introduccion.classList.remove('rotar-salida');
-        galeryproject.classList.remove('rotar-entrada');
-    }
+});
+
+btnGaleria.addEventListener('click', (e) => {
+    e.preventDefault();
+    mostrarProyectos();
+});
+
+btnInicio.addEventListener('click', (e) => {
+    e.preventDefault();
+    mostrarInicio();
 });
